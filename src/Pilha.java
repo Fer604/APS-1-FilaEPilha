833468b072a2834430ea6b4c28c8d1f6e533ecb1
andrééé <andreestevesarantes19@gmail.com>
2025-10-22 13:04:22 -0300

    pilhaaa

--- src/Pilha.java
+++ src/Pilha.java
@@ -1,5 +1,46 @@
 public class Pilha {
     private Node topo;
 
+    public void push(Elemento elemento) throws Exception {
+        if (elemento == null) {
+            throw new Exception("Elemento inválido");
+        }
+        Node novoNode = new Node(elemento);
+        novoNode.proximo = topo;
+        topo = novoNode;
+    }
 
+    public Elemento remover() throws Exception {
+        if (estaVazia()) {
+            throw new Exception("A pilha está vazia");
+        }
+        Elemento elementoRemovido = topo.valor;
+        topo = topo.proximo;
+        return elementoRemovido;
+    }
+
+    public boolean estaVazia() {
+        return (topo == null);
+    }
+
+    public void mostrarHistorico() {
+        if (estaVazia()) {
+            System.out.println("Histórico vazio");
+            return;
+        }
+
+        Node atual = topo;
+        int contador = 1;
+
+        while (atual != null) {
+            System.out.println("\nSolicitação " + contador + ":");
+            System.out.println("ID: " + atual.valor.id);
+            System.out.println("Descrição: " + atual.valor.descricao);
+            System.out.println("Data/Hora: " + atual.valor.dataHora);
+            System.out.println("-----------------");
+
+            atual = atual.proximo;
+            contador = contador + 1;
+        }
+    }
 }
